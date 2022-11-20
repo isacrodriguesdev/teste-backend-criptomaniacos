3a025a3a2a184ea8f294c081020f1f4619caff8b
Isac Rodrigues <isacrodriguesdev@protonmail.com>
2022-11-20 13:14:41 -0300

    feat: binance api controller adapter

---
+++ src/main/exchanges/Binance/BinanceControllerAdapter.ts
@@ -0,0 +1,23 @@
+import Binance, { Binance as IBinance } from "binance-api-node"
+import { User } from "../../../domain/entities/User";
+import { ICurrency, IExchangeController } from "../IExchangeController";
+
+export class BinanceControllerAdapter implements IExchangeController {
+
+  private getClient(user: User) {
+    return Binance({
+      apiKey: user.apiKey,
+      apiSecret: user.apiSecret
+    })
+  }
+
+  async getCurrencies(user: User): Promise<ICurrency[]> {
+    const account = await this.getClient(user).accountInfo()
+    return account.balances
+      .filter(({ free }) => parseFloat(free))
+      .map((coin) => ({
+        asset: coin.asset,
+        balance: coin.free
+      }))
+  }
+}
