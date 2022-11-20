1cd0650e7d6a72b9a5b4f4547b1c9fe9f83e9941
Isac Rodrigues <isacrodriguesdev@protonmail.com>
2022-11-20 15:25:50 -0300

    fix: incorrect file name

--- src/application/useCases/CreateWallet/CreateUserFactory.ts
+++
@@ -1,8 +0,0 @@
-import { PostgresWalletRepository } from "../../repositories/implementations/PostgresWalletRepository";
-import { CreateWalletUseCase } from "./CreateWalletUseCase";
-
-export function CreateWalletFactory() {
-  const postgresWalletRepository = new PostgresWalletRepository();
-
-  return new CreateWalletUseCase(postgresWalletRepository)
-}
